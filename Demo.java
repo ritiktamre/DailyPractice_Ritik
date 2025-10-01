a131d60d8f9629cdf994b7da3cdb554fd4077844
ritiktamre <ritiktamre@gmail.com>
2025-10-01 15:54:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,32 @@
+package BubbleSort;
+
+import java.util.Arrays;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+     int arr[] = {1,34,5,67,8,90,4,12,45};
+     
+     for(int i=0; i<arr.length-1 ; i++)
+     {
+    	 for(int j=0; j<arr.length-1-i; j++)
+    	 {
+    		 if(arr[j] > arr[j+1])
+    		 {
+    			 int temp = arr[j];
+    			 arr[j] = arr[j+1];
+    			 arr[j+1] = temp;
+    			 
+    			 
+    		 }
+    	 }
+     }
+     
+     for(int i=0;i<arr.length;i++)
+     {
+    	 System.out.println(arr[i]);
+     }
+	}
+
+}
